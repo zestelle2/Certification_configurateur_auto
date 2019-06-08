442da4963cc0dff2b22f83a222db8c75423e11d1
zestelle2 <asse.estelle@hotmail.fr>
2019-06-08 14:56:32 +0200

    fist commit symfony install + homePage

---
+++ public/filejs/app.js
@@ -0,0 +1,9 @@
+// var $ = require ('jquery');
+
+// // // permet que jQuery soit une variable gloabel car nous en avons besoin dans plusieurs fichier et non pas qu'avec webpack
+// global.$ = global.jQuery = $;
+
+require('./jquery.js.js.js');
+require('./popper.js.js.js');
+require('./fontawesome.js.js.js');
+require('./bootstrap.js.js.js');
