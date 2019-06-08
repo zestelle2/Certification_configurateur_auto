264b539a0f747663b4050732f9e0d1e5c87bbdd4
zestelle2 <asse.estelle@hotmail.fr>
2019-06-08 20:17:11 +0200

    homePage finished

--- src/Kernel.php
+++ src/Kernel.php
@@ -50,4 +50,6 @@ class Kernel extends BaseKernel
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
     }
+
+    
 }
